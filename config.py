2769ede61a80d917f16e0eaa26e3fb43d0993490
yanqian <yanqian0716@gmail.com>
2021-02-03 13:22:25 +0800

    .

--- config.py
+++ config.py
@@ -1,32 +1,32 @@
 # about dataset
 IMG_SIZE = 32
-BATCH_SIZE = 1
-LABEL_FILE_PATH = './dataset/label/data.csv'
-UNLABEL_FILE_PATH = './dataset/unlabel/data.csv'
+BATCH_SIZE = 32
+LABEL_FILE_PATH = '/content/food-11/labeled.csv'
+UNLABEL_FILE_PATH = '/content/food-11/unlabeled.csv'
 AUGMENT_MAGNITUDE = 16
-SHUFFLE_SIZE = BATCH_SIZE * 16
-DATA_LEN = 10  # 数据集的总长度
+SHUFFLE_SIZE = BATCH_SIZE * 4
+DATA_LEN = 9867  # 数据集的总长度
 
 # about model
 NUM_XLA_SHARDS = -1
 BATCH_NORM_EPSILON = 1e-3
 BATCH_NORM_DECAY = 0.99
 DROPOUT_RATE = 0.2
-NUM_CLASSES = 10
+NUM_CLASSES = 11
 
 # about training
 LOG_EVERY = 100
 SAVE_EVERY = 1000
 TEA_SAVE_PATH = './weights/T'
 STD_SAVE_PATH = './weights/S'
-MAX_EPOCHS = 1000
-MAX_STEPS = MAX_EPOCHS * (DATA_LEN / BATCH_SIZE)
+MAX_EPOCHS = 100
+MAX_STEPS = MAX_EPOCHS * (int(DATA_LEN / BATCH_SIZE)-1)
 
 UDA_WEIGHT = 8  # uda的权重
 UDA_STEPS = 5000
 
 # about UdaCrossEntroy
-UDA_DATA = 5  # 7
+UDA_DATA = 7
 LABEL_SMOOTHING = 0.15
 UDA_TEMP = 0.7
 UDA_THRESHOLD = 0.6
