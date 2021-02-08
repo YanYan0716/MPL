37cca2cfda7a882abe16d36191c5ddd78671049b
yanqian <yanqian0716@gmail.com>
2021-02-08 15:16:11 +0800

    .

--- train_single_model.py
+++ train_single_model.py
@@ -30,13 +30,12 @@ if __name__ == '__main__':
     # 构建模型
     teacher = Wrn28k(num_inp_filters=3, k=2)
 
-    # 定义损失函数，
+    # 损失函数，
     t_label_loss = tf.losses.CategoricalCrossentropy(
-        reduction=tf.keras.losses.Reduction.NONE,
         from_logits=True,
     )
 
-    # 定义student的学习率
+    # 学习率
     Tea_lr_fun = LearningRate(
         config.TEACHER_LR,
         config.TEACHER_LR_WARMUP_STEPS,
@@ -46,7 +45,10 @@ if __name__ == '__main__':
     global_step = 0
 
     for epoch in range(config.MAX_EPOCHS):
-        for batch_idx, (l_images, l_labels) in enumerate(ds_label_train):
+        for batch_idx, (data) in enumerate(ds_label_train):
+            l_images = data['images']
+            l_labels = data['labels']
+            global_step += 1
             with tf.GradientTape() as s_tape:
                 logits = teacher(x=l_images)  # shape=[8, 10]
                 cross_entroy = t_label_loss(
@@ -57,7 +59,7 @@ if __name__ == '__main__':
                 cross_entroy = tf.reduce_sum(cross_entroy) / \
                                          tf.convert_to_tensor(config.BATCH_SIZE, dtype=tf.float32)
                 SLOSS += cross_entroy
-            # 反向传播，更新student的参数-------
+            # 反向传播，更新参数-------
             TeacherLR = Tea_lr_fun.__call__(global_step=global_step)
             TeaOptim = keras.optimizers.SGD(
                 learning_rate=TeacherLR,
@@ -65,7 +67,7 @@ if __name__ == '__main__':
                 nesterov=True,
             )
             GStud_unlabel = s_tape.gradient(cross_entroy, teacher.trainable_variables)
-            GStud_unlabel, _ = tf.clip_by_global_norm(GStud_unlabel, config.GRAD_BOUND)
+            # GStud_unlabel, _ = tf.clip_by_global_norm(GStud_unlabel, config.GRAD_BOUND)
             TeaOptim.apply_gradients(zip(GStud_unlabel, teacher.trainable_variables))
 
             if (batch_idx + 1) % config.LOG_EVERY == 0:
@@ -74,11 +76,11 @@ if __name__ == '__main__':
                       + '/[SLoss: %.4f]' % SLOSS + ' [SLR: %.6f]' % TeacherLR)
                 SLOSS = 0
         # 测试student在test上的acc，当student开始训练的时候
-        if (TeacherLR > 0) and (epoch):
+        if (TeacherLR > 0) and (epoch%10==0):
             acc = test(teacher)
             print(f'testing ... acc: {acc}')
         # 保存weights
-        if ((epoch + 1) % config.SAVE_EVERY == 0) and (TeacherLR > 0):
-            Ssave_path = config.STD_SAVE_PATH + str(epoch + 1) + '_' + str(batch_idx + 1)
-            tf.saved_model.save(teacher, Ssave_path)
-            print(f'saving for epoch {epoch}, Spath:{Ssave_path}')
+        # if ((epoch + 1) % config.SAVE_EVERY == 0) and (TeacherLR > 0):
+        #     Ssave_path = config.STD_SAVE_PATH + str(epoch + 1) + '_' + str(batch_idx + 1)
+        #     tf.saved_model.save(teacher, Ssave_path)
+        #     print(f'saving for epoch {epoch}, Spath:{Ssave_path}')
