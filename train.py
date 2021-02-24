c272443892b3a849ff93f9f3526a65b75930a3c1
yanqian <yanqian0716@gmail.com>
2021-02-24 13:17:38 +0800

    .

--- train.py
+++ train.py
@@ -98,7 +98,10 @@ if __name__ == '__main__':
         TLOSS_3 = 0
         SLOSS = 0
         for batch_idx, (l_images, l_labels, ori_images, aug_images) in enumerate(ds_train):
-            global_step += 1
+            if epoch < 20:
+                global_step = 0
+            else:
+                global_step += 1
             all_images = tf.concat([l_images, ori_images, aug_images], axis=0)  # shape [15, 32, 32, 3]
             u_aug_and_l_images = tf.concat([aug_images, l_images], axis=0)
             # step1：经过teacher，得到输出
@@ -178,12 +181,17 @@ if __name__ == '__main__':
                 teacher_loss = cross_entroy['u'] * uda_weight + \
                                cross_entroy['l'] + \
                                cross_entroy['mpl'] * dot_product
+                if epoch < 20:
+                    teacher_loss = cross_entroy['l']+cross_entroy['mpl']*dot_product
+
                 TLOSS += teacher_loss
                 TLOSS_1 += (cross_entroy['u'] * uda_weight)
                 TLOSS_2 += cross_entroy['l']
                 TLOSS_3 += cross_entroy['mpl'] * dot_product
             # 反向传播，更新teacher的参数-------
             TeacherLR = Tea_lr_fun.__call__(global_step=global_step)
+            if epoch < 20:
+                TeacherLR = 0.005
             TeaOptim = tfa.optimizers.SGDW(
                 learning_rate=TeacherLR,
                 momentum=0.9,
