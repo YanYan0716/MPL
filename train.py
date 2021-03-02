efcb90298fe055c49e29eca8b91967bacf993bee
yanqian <yanqian0716@gmail.com>
2021-03-02 09:58:45 +0800

    .

--- train.py
+++ train.py
@@ -109,134 +109,132 @@ if __name__ == '__main__':
         SLOSS = 0
         for batch_idx, (l_images, l_labels, ori_images, aug_images) in enumerate(ds_train):
             global_step += 1
-            if global_step:
-                all_images = tf.concat([l_images, ori_images, aug_images], axis=0)  # shape [15, 32, 32, 3]
-                u_aug_and_l_images = tf.concat([aug_images, l_images], axis=0)
-                # step1：经过teacher，得到输出
-                with tf.GradientTape() as t_tape:
-                    output = teacher(all_images)  # shape=[15, 10]
-                    logits, labels, masks, cross_entroy = UdaCrossEntroy(output, l_labels, global_step)
-                # step2：1st call student -----------------------------
-                with tf.GradientTape() as s_tape:
-                    logits['s_on_aug_and_l'] = student(u_aug_and_l_images)  # shape=[8, 10]
-                    logits['s_on_u'], logits['s_on_l_old'] = tf.split(
-                        logits['s_on_aug_and_l'],
-                        [aug_images.shape[0], l_images.shape[0]],
-                        axis=0
-                    )
-                    cross_entroy['s_on_u'] = s_unlabel_loss(
-                        y_true=tf.stop_gradient(tf.nn.softmax(logits['aug'], -1)),
-                        y_pred=logits['s_on_u']
-                    )
-                    # 计算损失函数
-                    cross_entroy['s_on_u'] = tf.reduce_sum(cross_entroy['s_on_u']) / \
-                                             tf.convert_to_tensor(config.BATCH_SIZE * config.UDA_DATA, dtype=tf.float32)
-                    SLOSS += cross_entroy['s_on_u']
-                    # for taylor
-                    cross_entroy['s_on_l_old'] = s_label_loss(
-                        y_true=labels['l'],
-                        y_pred=logits['s_on_l_old']
-                    )
-
-                    cross_entroy['s_on_l_old'] = tf.reduce_sum(cross_entroy['s_on_l_old']) / \
-                                                 tf.convert_to_tensor(config.BATCH_SIZE, dtype=tf.float32)
-                # 反向传播，更新student的参数-------
-                StudentLR = Std_lr_fun.__call__(global_step=global_step)
-                StdOptim = keras.optimizers.SGD(
-                    learning_rate=StudentLR,
-                    momentum=0.9,
-                    nesterov=True,
-                    # weight_decay=5e-4,
+            all_images = tf.concat([l_images, ori_images, aug_images], axis=0)  # shape [15, 32, 32, 3]
+            u_aug_and_l_images = tf.concat([aug_images, l_images], axis=0)
+            # step1：经过teacher，得到输出
+            with tf.GradientTape() as t_tape:
+                output = teacher(all_images)  # shape=[15, 10]
+                logits, labels, masks, cross_entroy = UdaCrossEntroy(output, l_labels, global_step)
+            # step2：1st call student -----------------------------
+            with tf.GradientTape() as s_tape:
+                logits['s_on_aug_and_l'] = student(u_aug_and_l_images)  # shape=[8, 10]
+                logits['s_on_u'], logits['s_on_l_old'] = tf.split(
+                    logits['s_on_aug_and_l'],
+                    [aug_images.shape[0], l_images.shape[0]],
+                    axis=0
                 )
-                # StdOptim = keras.optimizers.Adam(learning_rate=StudentLR)
-                GStud_unlabel = s_tape.gradient(cross_entroy['s_on_u'], student.trainable_variables)
-                GStud_unlabel, _ = tf.clip_by_global_norm(GStud_unlabel, config.GRAD_BOUND)
-                StdOptim.apply_gradients(zip(GStud_unlabel, student.trainable_variables))
-                # step3: 2nd call student ------------------------------
-                logits['s_on_l_new'] = student(l_images)
-                cross_entroy['s_on_l_new'] = s_label_loss(
+                cross_entroy['s_on_u'] = s_unlabel_loss(
+                    y_true=tf.stop_gradient(tf.nn.softmax(logits['aug'], -1)),
+                    y_pred=logits['s_on_u']
+                )
+                # 计算损失函数
+                cross_entroy['s_on_u'] = tf.reduce_sum(cross_entroy['s_on_u']) / \
+                                         tf.convert_to_tensor(config.BATCH_SIZE * config.UDA_DATA, dtype=tf.float32)
+                SLOSS += cross_entroy['s_on_u']
+                # for taylor
+                cross_entroy['s_on_l_old'] = s_label_loss(
                     y_true=labels['l'],
-                    y_pred=logits['s_on_l_new']
+                    y_pred=logits['s_on_l_old']
                 )
-                cross_entroy['s_on_l_new'] = tf.reduce_sum(cross_entroy['s_on_l_new']) / \
-                                             tf.convert_to_tensor(config.BATCH_SIZE, dtype=config.DTYPE)
-                dot_product = cross_entroy['s_on_l_new'] - cross_entroy['s_on_l_old']
-                limit = 3.0**(0.5)
-                moving_dot_product = tf.random_uniform_initializer(minval=-limit, maxval=limit)(shape=dot_product.shape)
-                moving_dot_product = tf.Variable(initial_value=moving_dot_product, trainable=False, dtype=config.DTYPE)
-                moving_dot_product_update = moving_dot_product.assign_sub(0.01 * (moving_dot_product - dot_product))
-                dot_product = dot_product - moving_dot_product
-                dot_product = tf.stop_gradient(dot_product)
-                # step4: 求teacher的损失函数
-                with t_tape:
-                    # label = tf.math.argmax(tf.nn.softmax(logits['aug'], axis=-1), axis=-1)
-                    # label = tf.raw_ops.OneHot(indices=label, depth=config.NUM_CLASSES, on_value=1.0, off_value=0)
-                    cross_entroy['mpl'] = mpl_loss(
-                        # y_true=tf.stop_gradient(label),
-                        y_true=tf.stop_gradient(tf.nn.softmax(logits['aug'], axis=-1)),
-                        y_pred=logits['aug']
-                    )  # 恒正
-                    cross_entroy['mpl'] = tf.reduce_sum(cross_entroy['mpl']) / \
-                                          tf.convert_to_tensor(config.BATCH_SIZE * config.UDA_DATA, dtype=config.DTYPE)
-                    uda_weight = config.UDA_WEIGHT * tf.math.minimum(
-                        1., tf.cast(global_step, config.DTYPE) / float(config.UDA_STEPS)
-                    )
-                    # if StudentLR == 0:
-                    #     dot_product = 0
-                    teacher_loss = cross_entroy['u'] * uda_weight + \
-                                   cross_entroy['l'] + \
-                                   cross_entroy['mpl'] * dot_product
 
-                    TLOSS += teacher_loss
-                    TLOSS_1 += (cross_entroy['u'] * uda_weight)
-                    TLOSS_2 += cross_entroy['l']
-                    TLOSS_3 += cross_entroy['mpl'] * dot_product
-                # 反向传播，更新teacher的参数-------
-                TeacherLR = Tea_lr_fun.__call__(global_step=global_step)
-                TeaOptim = keras.optimizers.SGD(
-                    learning_rate=TeacherLR,
-                    momentum=0.9,
-                    nesterov=True,
-                    # weight_decay=5e-4,
+                cross_entroy['s_on_l_old'] = tf.reduce_sum(cross_entroy['s_on_l_old']) / \
+                                             tf.convert_to_tensor(config.BATCH_SIZE, dtype=tf.float32)
+            # 反向传播，更新student的参数-------
+            StudentLR = Std_lr_fun.__call__(global_step=global_step)
+            StdOptim = keras.optimizers.SGD(
+                learning_rate=StudentLR,
+                momentum=0.9,
+                nesterov=True,
+                # weight_decay=5e-4,
+            )
+            # StdOptim = keras.optimizers.Adam(learning_rate=StudentLR)
+            GStud_unlabel = s_tape.gradient(cross_entroy['s_on_u'], student.trainable_variables)
+            GStud_unlabel, _ = tf.clip_by_global_norm(GStud_unlabel, config.GRAD_BOUND)
+            StdOptim.apply_gradients(zip(GStud_unlabel, student.trainable_variables))
+            # step3: 2nd call student ------------------------------
+            logits['s_on_l_new'] = student(l_images)
+            cross_entroy['s_on_l_new'] = s_label_loss(
+                y_true=labels['l'],
+                y_pred=logits['s_on_l_new']
+            )
+            cross_entroy['s_on_l_new'] = tf.reduce_sum(cross_entroy['s_on_l_new']) / \
+                                         tf.convert_to_tensor(config.BATCH_SIZE, dtype=config.DTYPE)
+            dot_product = cross_entroy['s_on_l_new'] - cross_entroy['s_on_l_old']
+            limit = 3.0**(0.5)
+            moving_dot_product = tf.random_uniform_initializer(minval=-limit, maxval=limit)(shape=dot_product.shape)
+            moving_dot_product = tf.Variable(initial_value=moving_dot_product, trainable=False, dtype=config.DTYPE)
+            moving_dot_product_update = moving_dot_product.assign_sub(0.01 * (moving_dot_product - dot_product))
+            dot_product = dot_product - moving_dot_product
+            dot_product = tf.stop_gradient(dot_product)
+            # step4: 求teacher的损失函数
+            with t_tape:
+                # label = tf.math.argmax(tf.nn.softmax(logits['aug'], axis=-1), axis=-1)
+                # label = tf.raw_ops.OneHot(indices=label, depth=config.NUM_CLASSES, on_value=1.0, off_value=0)
+                cross_entroy['mpl'] = mpl_loss(
+                    # y_true=tf.stop_gradient(label),
+                    y_true=tf.stop_gradient(tf.nn.softmax(logits['aug'], axis=-1)),
+                    y_pred=logits['aug']
+                )  # 恒正
+                cross_entroy['mpl'] = tf.reduce_sum(cross_entroy['mpl']) / \
+                                      tf.convert_to_tensor(config.BATCH_SIZE * config.UDA_DATA, dtype=config.DTYPE)
+                uda_weight = config.UDA_WEIGHT * tf.math.minimum(
+                    1., tf.cast(global_step, config.DTYPE) / float(config.UDA_STEPS)
                 )
-                # TeaOptim = keras.optimizers.Adam(learning_rate=TeacherLR)
-                GTea = t_tape.gradient(teacher_loss, teacher.trainable_variables)
-                GTea, _ = tf.clip_by_global_norm(GTea, config.GRAD_BOUND)
-                TeaOptim.apply_gradients(zip(GTea, teacher.trainable_variables))
+                # if StudentLR == 0:
+                #     dot_product = 0
+                teacher_loss = cross_entroy['u'] * uda_weight + \
+                               cross_entroy['l'] + \
+                               cross_entroy['mpl'] * dot_product
 
-                if (batch_idx + 1) % config.LOG_EVERY == 0:
-                    TLOSS = TLOSS / config.LOG_EVERY
-                    TLOSS_1 = TLOSS_1 / config.LOG_EVERY
-                    TLOSS_2 = TLOSS_2 / config.LOG_EVERY
-                    TLOSS_3 = TLOSS_3 / config.LOG_EVERY
-                    SLOSS = SLOSS / config.LOG_EVERY
-                    print(f'global: %4d' % global_step + ',[epoch:%4d/' % epoch + 'EPOCH: %4d] \t' % config.MAX_EPOCHS
-                          + '[U:%.4f' % (TLOSS_1) + ', L:%.4f' % (TLOSS_2) + ', M:%.4f' % (
-                              TLOSS_3) + ']' + '[TLoss: %.4f]' % TLOSS + '/[SLoss: %.4f]' % SLOSS
-                          + '\t[TLR: %.6f' % TeacherLR + ']/[SLR: %.6f]' % StudentLR)
-                    TLOSS = 0
-                    TLOSS_1 = 0
-                    TLOSS_2 = 0
-                    TLOSS_3 = 0
-                    SLOSS = 0
-        if epoch > config.CONTINUE_EPOCH:
-            # 测试teacher在test上的acc
-            if epoch % 5 == 0:
-                Tacc = test(teacher)
-                print(f'testing teacher model ... acc: {Tacc}')
-            # 测试student在test上的acc，当student开始训练的时候
-            if (StudentLR > 0) and (epoch % 5 == 0):
-                acc = test(student)
-                print(f'testing ... acc: {acc}')
-            # 保存weights
-            if Tacc > TBacc:
-                Tsave_path = config.TEA_SAVE_PATH  # + str(epoch + 1) + '_' + str(batch_idx + 1)
-                teacher.save_weights(Tsave_path)
-                # tf.saved_model.save(teacher, Tsave_path)
-                TBacc = Tacc
-                print(f'saving for TBacc {TBacc}, Tpath:{Tsave_path}')
-            if Sacc > SBacc:
-                Ssave_path = config.STD_SAVE_PATH  # + str(epoch + 1) + '_' + str(batch_idx + 1)
-                student.save_weights(Ssave_path)
-                SBacc = Sacc
-                print(f'saving for SBacc {SBacc}, Spath:{Ssave_path}')
+                TLOSS += teacher_loss
+                TLOSS_1 += (cross_entroy['u'] * uda_weight)
+                TLOSS_2 += cross_entroy['l']
+                TLOSS_3 += cross_entroy['mpl'] * dot_product
+            # 反向传播，更新teacher的参数-------
+            TeacherLR = Tea_lr_fun.__call__(global_step=global_step)
+            TeaOptim = keras.optimizers.SGD(
+                learning_rate=TeacherLR,
+                momentum=0.9,
+                nesterov=True,
+                # weight_decay=5e-4,
+            )
+            # TeaOptim = keras.optimizers.Adam(learning_rate=TeacherLR)
+            GTea = t_tape.gradient(teacher_loss, teacher.trainable_variables)
+            GTea, _ = tf.clip_by_global_norm(GTea, config.GRAD_BOUND)
+            TeaOptim.apply_gradients(zip(GTea, teacher.trainable_variables))
+
+            if (batch_idx + 1) % config.LOG_EVERY == 0:
+                TLOSS = TLOSS / config.LOG_EVERY
+                TLOSS_1 = TLOSS_1 / config.LOG_EVERY
+                TLOSS_2 = TLOSS_2 / config.LOG_EVERY
+                TLOSS_3 = TLOSS_3 / config.LOG_EVERY
+                SLOSS = SLOSS / config.LOG_EVERY
+                print(f'global: %4d' % global_step + ',[epoch:%4d/' % epoch + 'EPOCH: %4d] \t' % config.MAX_EPOCHS
+                      + '[U:%.4f' % (TLOSS_1) + ', L:%.4f' % (TLOSS_2) + ', M:%.4f' % (
+                          TLOSS_3) + ']' + '[TLoss: %.4f]' % TLOSS + '/[SLoss: %.4f]' % SLOSS
+                      + '\t[TLR: %.6f' % TeacherLR + ']/[SLR: %.6f]' % StudentLR)
+                TLOSS = 0
+                TLOSS_1 = 0
+                TLOSS_2 = 0
+                TLOSS_3 = 0
+                SLOSS = 0
+        # 测试teacher在test上的acc
+        if epoch % 5 == 0:
+            Tacc = test(teacher)
+            print(f'testing teacher model ... acc: {Tacc}')
+        # 测试student在test上的acc，当student开始训练的时候
+        if (StudentLR > 0) and (epoch % 5 == 0):
+            acc = test(student)
+            print(f'testing ... acc: {acc}')
+        # 保存weights
+        if Tacc > TBacc:
+            Tsave_path = config.TEA_SAVE_PATH  # + str(epoch + 1) + '_' + str(batch_idx + 1)
+            teacher.save_weights(Tsave_path)
+            # tf.saved_model.save(teacher, Tsave_path)
+            TBacc = Tacc
+            print(f'saving for TBacc {TBacc}, Tpath:{Tsave_path}')
+        if Sacc > SBacc:
+            Ssave_path = config.STD_SAVE_PATH  # + str(epoch + 1) + '_' + str(batch_idx + 1)
+            student.save_weights(Ssave_path)
+            SBacc = Sacc
+            print(f'saving for SBacc {SBacc}, Spath:{Ssave_path}')
