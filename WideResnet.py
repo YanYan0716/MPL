3ecb75ea4269e7f66fe6a559d3bbf5f78e979074
yanqian <yanqian0716@gmail.com>
2021-03-10 13:22:30 +0800

    .

--- WideResnet.py
+++ WideResnet.py
@@ -27,7 +27,8 @@ class BasicBlock(layers.Layer):
             trainable=self.trainable,
             name=name+'_bn1'
         )
-        self.relu1 = layers.LeakyReLU(alpha=0.1)
+        # self.relu1 = layers.LeakyReLU(alpha=0.1)
+        self.relu1 = keras.activations.relu
         self.conv1 = layers.Conv2D(
             filters=self.out_channels,
             kernel_size=3,
@@ -44,7 +45,8 @@ class BasicBlock(layers.Layer):
             trainable=self.trainable,
             name=name+'_bn2'
         )
-        self.relu2 = layers.LeakyReLU(alpha=0.1)
+        # self.relu2 = layers.LeakyReLU(alpha=0.1)
+        self.relu2 = keras.activations.relu
         self.Drop = layers.Dropout(
             rate=self.dropout,
             trainable=self.trainable,
@@ -130,8 +132,8 @@ class WideResnet(keras.Model):
             trainable=self.trainable,
             name=name+'_bn1'
         )
-        self.relu1 = layers.LeakyReLU(alpha=0.1)
-
+        # self.relu1 = layers.LeakyReLU(alpha=0.1)
+        self.relu1 = keras.activations.relu
         self.avgpool = layers.GlobalAveragePooling2D(name=name+'_avgpool')
         self.dense = layers.Dense(
             units=config.NUM_CLASS,
