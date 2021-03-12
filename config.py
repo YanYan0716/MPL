8740ecfc6b7c37286051900a1761de4f16f3702c
yanqian <yanqian0716@gmail.com>
2021-03-12 11:22:43 +0800

    .

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 # about dataset
 IMG_SIZE = 32
 BATCH_SIZE = 64
-# LABEL_FILE_PATH = '/content/cifar/label4000.csv' # google
-# UNLABEL_FILE_PATH = '/content/cifar/train.csv'
+LABEL_FILE_PATH = '/content/cifar/label4000.csv' # google
+UNLABEL_FILE_PATH = '/content/cifar/train.csv'
 
 _MAX_LEVEL = 10
 CUTOUT_CONST = 40.
@@ -12,8 +12,8 @@ TRANSLATE_CONST = 100.
 REPLACE_COLOR = [128, 128, 128]
 
 
-LABEL_FILE_PATH = '../input/cifar10/cifar/label4000.csv'  # kaggle
-UNLABEL_FILE_PATH = '../input/cifar10/cifar/train.csv'
+# LABEL_FILE_PATH = '../input/cifar10/cifar/label4000.csv'  # kaggle
+# UNLABEL_FILE_PATH = '../input/cifar10/cifar/train.csv'
 
 
 AUGMENT_MAGNITUDE = 16
@@ -25,7 +25,7 @@ NUM_XLA_SHARDS = -1
 BATCH_NORM_EPSILON = 1e-5
 BATCH_NORM_DECAY = 0.999
 DROPOUT_RATE = 0.
-DROPOUT = 0.
+DROPOUT = 0.2
 NUM_CLASSES = 10
 NUM_CLASS = 10
 
@@ -38,7 +38,7 @@ STD_SAVE_PATH = './weights/S'
 MAX_EPOCHS = 19200
 MAX_STEPS = MAX_EPOCHS * (int(DATA_LEN / BATCH_SIZE)-1)
 UDA_WEIGHT = 8  # uda的权重
-UDA_STEPS = 10000
+UDA_STEPS = 20000
 TEST_EVERY = 2
 GRAD_BOUND = 1e9
 EMA = 0.995
@@ -47,14 +47,14 @@ EMA = 0.995
 # continue train
 TEA_CONTINUE = False
 STD_CONTINUE = False
-TEA_LOAD_PATH = '../input/weights/weights/T'
-STD_LOAD_PATH = '../input/weights/weights/S'
+# TEA_LOAD_PATH = '../input/weights/weights/T'
+# STD_LOAD_PATH = '../input/weights/weights/S'
 CONTINUE_EPOCH = 0
 
 
 # about testing
-# TEST_FILE_PATH = '/content/cifar/test.csv'
-TEST_FILE_PATH = '../input/cifar10/cifar/test.csv'
+TEST_FILE_PATH = '/content/cifar/test.csv'
+# TEST_FILE_PATH = '../input/cifar10/cifar/test.csv'
 TEST_MODEL_PATH = './weights/S'
 
 # about UdaCrossEntroy
@@ -66,7 +66,7 @@ UDA_THRESHOLD = 0.6
 # about learning rate
 STUDENT_LR = 0.005  # student
 STUDENT_LR_WARMUP_STEPS = 10000
-STUDENT_LR_WAIT_STEPS = 6000
+STUDENT_LR_WAIT_STEPS = 20000
 TEACHER_LR = 0.005  # teacher
 TEACHER_LR_WARMUP_STEPS = 10000
 TEACHER_NUM_WAIT_STEPS = 0
